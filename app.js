836a84f290b607521a797b1f7249494343a8dda4
dan <kind60027@gmail.com>
2018-11-12 03:19:34 +0800

    first_commit

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const moment = require('moment-timezone');
+const _ = require('lodash');
+const mongoose = require('mongoose');
+const  { User }  = require('./models/user.js');
+
+mongoose.connect('mongodb://localhost/ratingsystem', {
+    useMongoClient:true
+});
 
 var app = express();
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 
-app.get('/hello', (req,res) =>{
-    res.status(200).json({
-        message : "Hello word!!"
+app.post('/singup', (req,res) =>{
+    var body = _.pick(req.body,['email','password','name','phone','studentID','departmet','lineId','roleId'])
+    body.time = Date.now();
+    var user = new User(body);
+    user.save().then(()=>{
+        return user.generateToken();
+    }).then((token) =>{
+        res.header('authToken', token).send(user.name);
+    }).catch((error)=>{
+        res.status(400).send(error);
     })
+
 })
 
-app.listen(5000, () => {
+
+
+app.listen(3000, () => {
     console.log(moment().tz("Asia/Taipei").format());
     console.log("http//localhost:3000"); 
     
---
+++ models/user.js
@@ -0,0 +1,96 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const moment = require('moment-timezone');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+const {ObjectID} = require('mongodb');
+
+var UserSchema = new mongoose.Schema({
+    email:{
+        type : String,
+        required: true,
+        trim: true,
+        minlength: 1,
+        unique: true,
+        validator: {
+            validator:(value)=>{validator.isEmail(value)},
+            message:'不是合法信箱'
+        }
+    },
+    password:{
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    name:{
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    phone:{
+        type: String,
+        required: true,
+        minlength: 10
+    },
+    time:{
+        type: Date
+    },
+    studentID:{
+        type: String,
+        required: true
+    },
+    departmet:{
+        type: String,
+        required: true
+    },
+    lineId:{
+        type:String
+    },
+    roleId:{
+        type: String,
+        required: true
+    },
+    tokens:[{
+        access:{
+            type: String,
+            required: true,
+        },
+        token:{
+            type:String,
+            required:true
+        }
+    }]
+
+})
+
+UserSchema.methods.generateToken = function(){
+    var user = this;
+    var access = user.roleId;
+    var token =  jwt.sign({_id:user._id.toHexSting(),access},'abc123').toString();
+    console.log("1");
+    user.tokens.push({access,token});
+    return user.save().then(()=>{
+        console.log("2");
+        return token
+        console.log("3");
+    })
+}
+
+UserSchema.pre('save', function(next){
+    var user =this;
+    if(user.isModified('password')){
+        bcrypt.hash(user.password,10).then(hash =>{
+            user.password = hash;
+            next(); 
+        })      
+    }else{
+        next();
+    }   
+})
+
+var User = mongoose.model('user', UserSchema);
+
+module.exports = {
+    User
+}
